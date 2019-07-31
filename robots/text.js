e15d159ab44feec53d17d733366cc035dc421bf7
Caio Cesar <caio.c.dev@gmail.com>
2019-07-30 22:59:50 -0300

    feat: Get interpretation of sentences from watson, using nlu - natural language understanding

--- robots/text.js
+++ robots/text.js
@@ -2,15 +2,33 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway-syd.watsonplatform.net/natural-language-understanding/api'
+});
+
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     brackContentIntoSentences(content)
+    limitiMaximumSentences(content)
+    await fetchKeywordsOffAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+
+        // Pesquina na wiki Portugues
+        // const wikipediaResponse = await wikipediaAlgorithm.pipe({ articleName: content.searchTerm, lang: "pt" })
+
+        //Pesquisa na wiki ingles
         const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
@@ -51,8 +69,39 @@ async function robot(content) {
                 images: []
             })
         })
+    }
 
-        console.log(content.sentences)
+    function limitiMaximumSentences(content) {
+        // console.log("Antes=>", content.sentences.length)
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+        // console.log("Depois=>", content.sentences.length)
+    }
+
+    async function fetchKeywordsOffAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+
+                resolve(keywords)
+            })
+        })
     }
 }
 
